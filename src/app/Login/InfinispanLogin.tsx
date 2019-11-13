7a27e6835379745e59dee107c50b510ff9dfa31b
Katia Aresti <karesti@redhat.com>
2019-11-13 14:54:59 +0100

    some login change

--- src/app/Login/InfinispanLogin.tsx
+++ src/app/Login/InfinispanLogin.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {LoginForm, LoginPage, PageSection, Title} from '@patternfly/react-core';
+import icon from '!!url-loader!@app/assets/images/infinispan_icon.svg';
 
 /**
  * Note: When using background-filter.svg, you must also include #image_overlay as the fragment identifier
@@ -8,19 +9,14 @@ import {LoginForm, LoginPage, PageSection, Title} from '@patternfly/react-core';
 const images = {};
 
 const InfinispanLogin: React.FunctionComponent<any> = (props) => {
+
   return (
       <LoginPage footerListVariants="inline"
-                 brandImgAlt="PatternFly logo"
-                 backgroundImgAlt="Images"
-                 textContent="This is placeholder text only. Use this area to place any information or introductory message about your
-        application that may be relevant to users."
-                 loginTitle="Log in to your account"
-                 loginSubtitle="Please use your single sign-on LDAP credentials">
+                 brandImgSrc={icon}
+                 loginTitle="Log in to Infinispan server">
         <LoginForm
           usernameLabel="Username"
-          usernameValue="this.state.usernameValue"
           passwordLabel="Password"
-          rememberMeLabel="Keep me logged in for 30 days."
         />
 
       </LoginPage>
