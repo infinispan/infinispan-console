6fe98405a53f1eea1bf6d5fde2f39946fcda6301
Katia Aresti <karesti@redhat.com>
2019-12-03 16:08:31 +0100

    refactor display of cluster members

--- src/app/CacheManagers/ClusterDisplay.tsx
+++ src/app/CacheManagers/ClusterDisplay.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import {
   Card,
   CardBody,
-  CardHeader,
   EmptyState,
   EmptyStateBody,
   EmptyStateIcon,
   EmptyStateVariant,
+  Gallery,
+  GalleryItem,
+  Grid,
+  GridItem,
   Label,
   Stack,
   StackItem
@@ -14,7 +17,7 @@ import {
 import {ClusterIcon, CubesIcon} from "@patternfly/react-icons";
 import displayUtils from "../../services/displayUtils";
 
-const ClusterDisplay = (props: { cacheManager: (undefined |CacheManager) }) => {
+const ClusterDisplay = (props: { cacheManager: (undefined | CacheManager) }) => {
   const cm: (undefined | CacheManager) = props.cacheManager;
 
   if (!cm) {
@@ -27,20 +30,30 @@ const ClusterDisplay = (props: { cacheManager: (undefined |CacheManager) }) => {
       </EmptyState>
     );
   }
-
+  let size = cm.cluster_size > 1 ? cm.cluster_size + ' members' : cm.cluster_size + ' member';
   return (
     <Card style={{marginTop: 40}}>
-      <CardHeader><ClusterIcon/> <strong>{' ' + cm.cluster_name}</strong></CardHeader>
       <CardBody>
-        <Stack gutter="md">
-          <StackItem><Label
-            style={{backgroundColor: displayUtils.healthColor(cm.health)}}>{cm.health}</Label></StackItem>
-          <StackItem>Size <strong>{cm.cluster_size}</strong></StackItem>
-          <StackItem><strong>{cm.cluster_members.map(mem => <span key={mem}
-                                                                        style={{marginRight: 10}}>[{mem}]</span>)}</strong></StackItem>
-          <StackItem><strong>{cm.cluster_members_physical_addresses.map(add => <span key={add}
-                                                                                           style={{marginRight: 10}}>[{add}]</span>)}</strong></StackItem>
-        </Stack>
+        <Grid gutter="md">
+          <GridItem span={2}>
+            <Stack>
+              <StackItem><ClusterIcon size={"xl"} style={{marginLeft:14, marginBottom: 10}}/></StackItem>
+              <StackItem><Label
+                style={{backgroundColor: displayUtils.healthColor(cm.health)}}>{cm.health}</Label></StackItem>
+            </Stack>
+          </GridItem>
+          <GridItem span={4}>
+            <Stack>
+              <StackItem> <h1 style={{fontSize: 22}}>{cm.cluster_name}</h1></StackItem>
+              <StackItem> {size}</StackItem>
+            </Stack>
+          </GridItem>
+          <GridItem span={6}>
+            <Gallery>
+              {cm.cluster_members.map(mem => <GalleryItem>{mem}</GalleryItem>)}
+            </Gallery>
+          </GridItem>
+        </Grid>
       </CardBody>
     </Card>
   );
