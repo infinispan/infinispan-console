98cd27099f5817837cc6eddc4c3cc9175748922a
Katia Aresti <karesti@redhat.com>
2019-12-04 16:47:31 +0100

    remove login

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,23 +2,18 @@ import * as React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
 import {BrowserRouter as Router} from 'react-router-dom';
 import {AppLayout} from '@app/AppLayout/AppLayout';
-import {AppRoutes, isLogged} from '@app/routes';
+import {AppRoutes} from '@app/routes';
 import DocumentTitle from 'react-document-title';
 import '@app/app.css';
-import InfinispanLogin from "@app/Login/InfinispanLogin";
 
-const App: React.FunctionComponent = () => {
-  if (!isLogged) {
-    return <InfinispanLogin/>
-  } else {
-    return <DocumentTitle title="Infinispan Console">
-      <Router basename="/console">
-        <AppLayout>
-          <AppRoutes/>
-        </AppLayout>
-      </Router>
-    </DocumentTitle>
-  }
+const App: React.FunctionComponent<any> = () => {
+  return <DocumentTitle title="Infinispan Console">
+    <Router basename="/console">
+      <AppLayout>
+        <AppRoutes/>
+      </AppLayout>
+    </Router>
+  </DocumentTitle>
 };
 
 export {App};
