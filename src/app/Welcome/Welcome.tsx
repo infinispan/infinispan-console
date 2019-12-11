4021a72b9aa8e6cab618b276a914279d4410c515
Katia Aresti <karesti@redhat.com>
2019-12-11 16:24:24 +0100

    ISPN-11069 swap welcome page content

--- src/app/Welcome/Welcome.tsx
+++ src/app/Welcome/Welcome.tsx
@@ -18,10 +18,27 @@ const Welcome: React.FunctionComponent<any> = (props) => {
 
   const brandname = 'Infinispan';
 
-  const description = 'Infinispan is a distributed in-memory key/value data store with optional schema, available under the Apache License 2.0. ' +
-    'Available as an embedded Java library or as a language-independent service accessed remotely over a variety of protocols (Hot Rod, REST)'
-    + 'Use it as a cache or a data grid. Works on bare-metal, containerized and virtualized environments. ' +
+  const description1 = 'Infinispan is a distributed in-memory key/value data store with optional schema, available under the Apache License 2.0. ' +
+    'Available as an embedded Java library or as a language-independent service accessed remotely over a variety of protocols (Hot Rod, REST)';
+
+  const description2 = 'Use it as a cache or a data grid. Works on bare-metal, containerized and virtualized environments. ' +
     'Works on AWS, Azure, Google Cloud, Kubernetes and OpenShift.';
+
+
+  const FooterList = () => {
+    return  <Stack>
+      <StackItem>Remotely connect to Infinispan with client applications.</StackItem>
+      <StackItem><Button component="a" href="https://infinispan.org/hotrod-clients/" variant="link"
+                         icon={<CatalogIcon/>}>Download a client</Button></StackItem>
+      <StackItem>Find out how to set up, configure, and monitor {brandname} servers.</StackItem>
+      <StackItem><Button component="a" href="https://infinispan.org/about/" variant="link" icon={<InfoIcon/>}>Learn
+        more</Button></StackItem>
+      <StackItem>Develop applications to use {brandname} for in-memory storage in a variety of use
+        cases.</StackItem>
+      <StackItem><Button component="a" href="https://github.com/infinispan/infinispan-simple-tutorials"
+                         variant="link" icon={<ApplicationsIcon/>}>Run the simple tutorials </Button></StackItem>
+    </Stack>
+  };
 
   return (
     <Page>
@@ -29,24 +46,16 @@ const Welcome: React.FunctionComponent<any> = (props) => {
       <LoginPage
         footerListVariants="inline"
         brandImgSrc={icon}
-        brandImgAlt="PatternFly logo"
+        brandImgAlt="Infinispan logo"
         backgroundImgSrc={images}
-        backgroundImgAlt="Images"
-        textContent={description}
+        backgroundImgAlt={brandname}
         loginTitle="Welcome to the Infinispan Server"
-        footerListItems={<Button href={utils.endpoint() + '/login?action=login'} component="a" variant={"control"}>Go to the console</Button>}
+        footerListItems={<FooterList/>}
       >
         <Stack gutter={"md"}>
-          <StackItem>Remotely connect to Infinispan with client applications.</StackItem>
-          <StackItem><Button component="a" href="https://infinispan.org/hotrod-clients/" variant="link"
-                             icon={<CatalogIcon/>}>Download a client</Button></StackItem>
-          <StackItem>Find out how to set up, configure, and monitor {brandname} servers.</StackItem>
-          <StackItem><Button component="a" href="https://infinispan.org/about/" variant="link" icon={<InfoIcon/>}>Learn
-            more</Button></StackItem>
-          <StackItem>Develop applications to use {brandname} for in-memory storage in a variety of use
-            cases.</StackItem>
-          <StackItem><Button component="a" href="https://github.com/infinispan/infinispan-simple-tutorials"
-                             variant="link" icon={<ApplicationsIcon/>}>Run the simple tutorials </Button></StackItem>
+          <StackItem>{description1}</StackItem>
+          <StackItem>{description2}</StackItem>
+          <StackItem><Button href={utils.endpoint() + '/login?action=login'} component="a">Go to the console</Button></StackItem>
         </Stack>
       </LoginPage>
     </Page>
