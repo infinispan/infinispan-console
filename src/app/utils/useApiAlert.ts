a7179cf6bce651e3831f2ccca00e32fb05d2d359
Katia Aresti <karesti@redhat.com>
2020-05-04 15:20:58 +0200

    ISPN-11705 Global alert system * Uses useContext * Custom hook * For one, one alert at a time * Create and Delete refactored

---
+++ src/app/utils/useApiAlert.ts
@@ -0,0 +1,8 @@
+import {useContext} from 'react';
+import {APIAlertContext} from '../providers/APIAlertProvider';
+
+// a custom hook for using the alert context
+export function useApiAlert() {
+  const {alert, addAlert, removeAlert} = useContext(APIAlertContext);
+  return {alert, addAlert, removeAlert};
+}
