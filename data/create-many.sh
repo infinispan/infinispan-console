282656fc191571a4b7bea6c85ddba36353829c83
Katia Aresti <karesti@redhat.com>
2020-05-05 11:56:35 +0200

    pass arguments to create data  scripts

--- data/create-many.sh
+++ data/create-many.sh
@@ -1,34 +1,38 @@
+user=$1
+pass=$2
+userPass="$user:$pass"
+
 echo "= Init create many data"
 echo "= Delete caches"
 for i in {1..500}
 do
-curl -XDELETE  http://localhost:11222/rest/v2/caches/users-$i
+curl -XDELETE  -u $userPass http://localhost:11222/rest/v2/caches/users-$i
 done
 
 echo "= Create caches"
 for i in {1..500}
 do
-curl -XPOST  http://localhost:11222/rest/v2/caches/users-$i?template=org.infinispan.DIST_ASYNC
+curl -XPOST -u $userPass http://localhost:11222/rest/v2/caches/users-$i?template=org.infinispan.DIST_ASYNC
 done
 
 echo "= Delete counters"
 for i in {1..500}
 do
-curl -XDELETE http://localhost:11222/rest/v2/counters/weak-$i
-curl -XDELETE http://localhost:11222/rest/v2/counters/strong-$i
+curl -XDELETE -u $userPass http://localhost:11222/rest/v2/counters/weak-$i
+curl -XDELETE -u $userPass http://localhost:11222/rest/v2/counters/strong-$i
 done
 
 echo "= Create some counters"
 for i in {1..500}
 do
-  curl -XPOST -H "Content-Type: application/json" -d "@weakCounter.json" "http://localhost:11222/rest/v2/counters/weak-$i"
-  curl -XPOST -H "Content-Type: application/json" -d "@strongCounter.json" "http://localhost:11222/rest/v2/counters/strong-$i"
+  curl -XPOST -u $userPass -H "Content-Type: application/json" -d "@weakCounter.json" "http://localhost:11222/rest/v2/counters/weak-$i"
+  curl -XPOST -u $userPass  -H "Content-Type: application/json" -d "@strongCounter.json" "http://localhost:11222/rest/v2/counters/strong-$i"
 done
 
 echo "= Create some tasks"
 for i in {1..1000}
 do
- curl -XPOST -H "Content-Type: application/javascript" -d "@hello.js" "http://localhost:11222/rest/v2/tasks/hello$i"
+ curl -XPOST -u $userPass -H "Content-Type: application/javascript" -d "@hello.js" "http://localhost:11222/rest/v2/tasks/hello$i"
 done
 
 echo "= End"
