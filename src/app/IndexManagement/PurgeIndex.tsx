1bc8765682f6d66e93d1db84291250f5ab149137
Katia Aresti <karesti@redhat.com>
2020-06-02 15:20:37 +0200

    ISPN-11657 Purge index

---
+++ src/app/IndexManagement/PurgeIndex.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  Button,
+  ButtonVariant,
+  Modal,
+  TextContent,
+  Text
+} from '@patternfly/react-core';
+import cacheService from '../../services/cacheService';
+import { useApiAlert } from '@app/utils/useApiAlert';
+import { useRecentActivity } from '@app/utils/useRecentActivity';
+
+/**
+ * Purge index modal
+ */
+const PurgeIndex = (props: {
+  cacheName: string;
+  isModalOpen: boolean;
+  closeModal: () => void;
+}) => {
+  const { addAlert } = useApiAlert();
+  const { pushActivity } = useRecentActivity();
+
+  const onClickPurgeButton = () => {
+    cacheService
+      .purgeIndexes(props.cacheName)
+      .then(actionResponse => {
+        props.closeModal();
+        addAlert(actionResponse);
+      });
+  };
+
+  return (
+    <Modal
+      className="pf-m-redhat-font"
+      width={'50%'}
+      isOpen={props.isModalOpen}
+      title={'Purge index?'}
+      onClose={props.closeModal}
+      isFooterLeftAligned
+      aria-label="Purge index modal"
+      actions={[
+        <Button
+          key="purge"
+          variant={ButtonVariant.danger}
+          onClick={onClickPurgeButton}
+        >
+          Purge
+        </Button>,
+        <Button key="cancel" variant="link" onClick={props.closeModal}>
+          Cancel
+        </Button>
+      ]}
+    >
+      <TextContent>
+        <Text>
+          This action will permanently clear all indexes {' '}
+          from the cache{' '} <strong>{props.cacheName}</strong>.
+          <br />
+          This cannot be undone.
+        </Text>
+      </TextContent>
+    </Modal>
+  );
+};
+
+export { PurgeIndex };
