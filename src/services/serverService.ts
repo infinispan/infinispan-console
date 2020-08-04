2c308f435dbdbf199d46d015ae29e085ea7c0626
Katia Aresti <karesti@redhat.com>
2020-08-04 19:14:49 +0200

    ISPN-12195 About page for Infinispan Console

---
+++ src/services/serverService.ts
@@ -0,0 +1,44 @@
+import utils from './utils';
+import { Either, left, right } from './either';
+import displayUtils from './displayUtils';
+
+class ServerService {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  /**
+   * Get server version or an error
+   */
+  public async getVersion(): Promise<Either<string, string>> {
+    return utils
+      .restCall(this.endpoint, 'GET')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then(json => right(json.version))
+      .catch(err => {
+        if (err instanceof TypeError) {
+          return left(err.message);
+        }
+        return err.text().then(errorMessage => {
+          let message = 'Unable to retrieve server version ' + name;
+          if (errorMessage.length > 0) {
+            message = errorMessage;
+          }
+          return left(message);
+        });
+      });
+  }
+}
+
+const serverService: ServerService = new ServerService(
+  utils.endpoint() + '/server'
+);
+
+export default serverService;
