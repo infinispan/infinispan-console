36f61b40c2524df1bd772da6f713c478d93104e3
Prajakta Deshmukh <pradeshm@localhost.localdomain>
2021-01-18 13:34:54 +0100

    glabal stats href tests

--- cypress/integration/global-stats-spec.js
+++ cypress/integration/global-stats-spec.js
@@ -11,4 +11,39 @@ describe('Global stats', () => {
        cy.get('h5').should('contain', 'Operations Performance')
        cy.get('h5').should('contain', 'Cache Manager Lifecycle')
     })
+
+  //View all caches href
+  it('successfully loads Global stats', () => {
+       cy.visit('http://localhost:11222/console/global-stats', {
+                headers: {
+                  "Accept-Encoding": "gzip, deflate, br"
+                 }
+       });
+        //click View all caches should navigate to console page
+        cy.get('.pf-m-2-row .pf-c-button').click();
+        //back to cluster-membership
+        cy.visit('http://localhost:11222/console/', {
+        headers: {
+                  "Accept-Encoding": "gzip, deflate, br"
+                 }
+        });
+   })
+
+  //View Cluster Status href
+  it('successfully loads Global stats', () => {
+        cy.visit('http://localhost:11222/console/global-stats', {
+                 headers: {
+                    "Accept-Encoding": "gzip, deflate, br"
+                  }
+        });
+        //click View Cluster Status should navigate to cluster-membership page
+        cy.get('.pf-l-grid__item:nth-child(3) .pf-c-button').click();
+        //back to cluster-membership
+        cy.visit('http://localhost:11222/console/cluster-membership', {
+        headers: {
+                  "Accept-Encoding": "gzip, deflate, br"
+                 }
+        });
+  })
+
 });
