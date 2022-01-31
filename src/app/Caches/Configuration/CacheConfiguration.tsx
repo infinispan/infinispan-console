4504a86a87085c663d9135c8e6886db6acc40976
Katia Aresti <karesti@redhat.com>
2022-02-01 00:53:53 +0100

    ISPN-13353 View configuration in json, xml and yaml
    * Custom Hook for fetching configuration in YAML, XML format
    * Changed props in CacheConfiguration.tsx file
    * Unit test CacheConfiguration.test.tsx added
    * Formatter for XML added

--- src/app/Caches/Configuration/CacheConfiguration.tsx
+++ src/app/Caches/Configuration/CacheConfiguration.tsx
@@ -1,63 +1,105 @@
 import React from 'react';
 import {
-  Alert, AlertActionLink, AlertVariant, Button,
+  Alert,
+  AlertActionLink,
+  AlertVariant,
   Card,
-  CardBody, CardHeader,
+  CardBody,
+  CardHeader,
   ClipboardCopy,
   ClipboardCopyVariant,
   Spinner,
+  Tabs,
+  Tab,
+  TabTitleText,
 } from '@patternfly/react-core';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { githubGist } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { useTranslation } from 'react-i18next';
+import {
+  useFetchConfigurationYAML,
+  useFetchConfigurationXML,
+} from '@app/services/configHook';
 
 const CacheConfiguration = (props: {
-  cache: DetailedInfinispanCache | undefined;
+  cacheName: string;
+  editable: boolean;
+  config: string;
 }) => {
   const { t } = useTranslation();
   const encodingDocs = t('brandname.encoding-docs-link');
-  if (!props.cache) {
-    return (
-      <Spinner size={'md'} />
-    );
+
+  const yamlConfig = useFetchConfigurationYAML(props.cacheName);
+  const xmlConfig = useFetchConfigurationXML(props.cacheName);
+
+  if (!props.config) {
+    return <Spinner size={'md'} />;
   }
 
   const displayHeaderAlert = () => {
-    if(props.cache?.editable) {
+    if (props.editable) {
       return '';
     }
 
     return (
       <CardHeader>
-        <Alert isInline title={t('caches.configuration.no-encoding-warning')}
-               variant={AlertVariant.warning}
-               actionLinks={
-                   <AlertActionLink onClick={() => window.open(encodingDocs, "_blank")}>
-                     {t('caches.configuration.no-encoding-docs')}</AlertActionLink>
-               }
+        <Alert
+          isInline
+          title={t('caches.configuration.no-encoding-warning')}
+          variant={AlertVariant.warning}
+          actionLinks={
+            <AlertActionLink
+              onClick={() => window.open(encodingDocs, '_blank')}
+            >
+              {t('caches.configuration.no-encoding-docs')}
+            </AlertActionLink>
+          }
         />
-
       </CardHeader>
     );
   };
 
+  const displayConfig = (config: string) => {
+    if (!config) {
+      return <Spinner size={'sm'} />;
+    }
+
+    return (
+      <Card>
+        {displayHeaderAlert()}
+        <CardBody>
+          <SyntaxHighlighter
+            wrapLines={false}
+            style={githubGist}
+            useInlineStyles={true}
+            showLineNumbers={true}
+          >
+            {config}
+          </SyntaxHighlighter>
+          <ClipboardCopy
+            isReadOnly
+            isCode
+            variant={ClipboardCopyVariant.inline}
+          >
+            {config}
+          </ClipboardCopy>
+        </CardBody>
+      </Card>
+    );
+  };
+
   return (
-    <Card>
-      {displayHeaderAlert()}
-      <CardBody>
-        <SyntaxHighlighter
-          wrapLines={false}
-          style={githubGist}
-          useInlineStyles={true}
-          showLineNumbers={true}
-        >
-          {props.cache.configuration.config}
-        </SyntaxHighlighter>
-        <ClipboardCopy isReadOnly isCode variant={ClipboardCopyVariant.inline}>
-          {props.cache.configuration.config}
-        </ClipboardCopy>
-      </CardBody>
-    </Card>
+    <Tabs defaultActiveKey={0} style={{ backgroundColor: 'white' }}>
+      <Tab eventKey={0} title={<TabTitleText>JSON</TabTitleText>}>
+        {displayConfig(props.config)}
+      </Tab>
+      <Tab eventKey={1} title={<TabTitleText>XML</TabTitleText>}>
+        {xmlConfig && displayConfig(xmlConfig.configuration)}
+      </Tab>
+      <Tab eventKey={2} title={<TabTitleText>YAML</TabTitleText>}>
+        {yamlConfig && displayConfig(yamlConfig.configuration)}
+      </Tab>
+    </Tabs>
   );
 };
 
