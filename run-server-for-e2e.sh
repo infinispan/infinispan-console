86787836c86464b1f8865d8cb01a9899d2763fb2
Katia Aresti <karesti@redhat.com>
2022-03-02 10:11:34 +0100

    14.0.0.Dev01

--- run-server-for-e2e.sh
+++ run-server-for-e2e.sh
@@ -9,7 +9,7 @@ else
 fi
 
 
-SERVER_VERSION="13.0.5.Final"
+SERVER_VERSION="14.0.0.Dev01"
 SERVER_HOME=server/infinispan-server-$SERVER_VERSION
 CLUSTER_SIZE_MAIN="$SERVER_HOME/bin/cli.sh -c localhost:11322 -f batch "
 ZIP_ROOT="http://downloads.jboss.org/infinispan"
