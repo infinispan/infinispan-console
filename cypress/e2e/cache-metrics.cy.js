84dc6dd397d153eaba1ba4240a1c309f6ddbd9ce
dipguptaredhat <dipgupta@redhat.com>
2022-10-05 01:03:00 +0530

    ISPN-11660 Updated e2e test for cache-metrics

--- cypress/e2e/cache-metrics.cy.js
+++ cypress/e2e/cache-metrics.cy.js
@@ -3,19 +3,18 @@ describe('Cache Metrics Overview', () => {
       cy.login(Cypress.env("username"), Cypress.env("password"), '/cache/people');
       //Check for Labels
       cy.contains('Metrics (Enabled)').click();
-      cy.contains('Current number of entries');
+      cy.contains('Approximate number of entries');
       cy.contains('Minimum number of nodes');
-      cy.contains('Size of cache in off-heap memory');
-      cy.contains('Size of cache in heap memory');
 
       // Check for data greater than -1
-      cy.get('dt[aria-label="view-cache-metrics-size"]').invoke('text').then(parseFloat).should('be.gt', -1)
+      cy.get('dt[aria-label="view-cache-approximate-unique-entries"]').invoke('text').then(parseFloat).should('be.gt', -1)
       cy.get('dt[aria-label="view-cache-metrics-nodes"]').invoke('text').then(parseFloat).should('be.gt', -1)
     })
 
     it('successfully checks cache metrics for heap memory', () => {
       cy.login(Cypress.env("username"), Cypress.env("password"), '/cache/heap-test');
       cy.contains('Metrics (Enabled)').click();
+      cy.contains('Size of cache in heap memory');
       cy.get('dt[aria-label="view-cache-metrics-heap"]').invoke('text').then(parseFloat).should('be.gt', -1)
 
     })
@@ -23,6 +22,7 @@ describe('Cache Metrics Overview', () => {
     it('successfully checks cache metrics for off-heap memory', () => {
       cy.login(Cypress.env("username"), Cypress.env("password"), '/cache/off-heap-test');
       cy.contains('Metrics (Enabled)').click();
+      cy.contains('Size of cache in off-heap memory');
       cy.get('dt[aria-label="view-cache-metrics-off-heap"]').invoke('text').then(parseFloat).should('be.gt', -1)
     })
 
