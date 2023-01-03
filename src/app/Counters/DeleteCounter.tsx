44b3dd109e1826bca61d0412346751abb835e283
Dipanshu Gupta <dipgupta@redhat.com>
2023-01-03 10:46:08 +0530

    ISPN-14298 Delete a counter

--- src/app/Counters/DeleteCounter.tsx
+++ src/app/Counters/DeleteCounter.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Button, ButtonVariant, Modal, Text, TextContent } from '@patternfly/react-core';
 import { useDeleteCounter } from '@app/services/countersHook';
+import { useTranslation } from 'react-i18next';
+import { ConsoleServices } from '@services/ConsoleServices';
+import { ConsoleACL } from '@services/securityService';
+import { useConnectedUser } from '@app/services/userManagementHook';
 
 /**
  * Delete counter modal
  */
 const DeleteCounter = (props: { name: string; isModalOpen: boolean; closeModal: () => void }) => {
+  const { t } = useTranslation();
+
   const { onDelete } = useDeleteCounter(props.name);
+  const { connectedUser } = useConnectedUser();
+  const canDelete = ConsoleServices.security().hasConsoleACL(ConsoleACL.ADMIN, connectedUser);
 
   return (
     <Modal
@@ -15,24 +23,25 @@ const DeleteCounter = (props: { name: string; isModalOpen: boolean; closeModal: 
       className="pf-m-redhat-font"
       width={'50%'}
       isOpen={props.isModalOpen}
-      title={'Permanently delete counter?'}
+      title={t('cache-managers.counters.modal-delete-modal')}
       onClose={props.closeModal}
       aria-label="Delete counter modal"
       disableFocusTrap={true}
       actions={[
         <Button
-          key={'Confirm'}
+          key={t('cache-managers.counters.modal-confirm-button')}
           aria-label={'Confirm'}
           variant={ButtonVariant.danger}
           onClick={() => {
             onDelete();
             props.closeModal();
           }}
+          isDisabled={!canDelete}
         >
-          Delete
+          {t('cache-managers.counters.delete-action')}
         </Button>,
         <Button key={'Cancel'} aria-label={'Cancel'} variant={ButtonVariant.link} onClick={props.closeModal}>
-          Cancel
+          {t('cache-managers.counters.modal-cancel-button')}
         </Button>
       ]}
     >
