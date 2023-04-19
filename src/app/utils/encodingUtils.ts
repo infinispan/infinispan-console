071ccc1b1acbf57e0db2206392dc7727e8088ef2
Dipanshu Gupta <dipgupta@redhat.com>
2023-04-19 14:31:22 +0530

    ISPN-14682 Do not retrieve entries when no encoding in cache * added brandname for encoding message

---
+++ src/app/utils/encodingUtils.ts
@@ -0,0 +1,5 @@
+import { EncodingType } from '@services/infinispanRefData';
+
+export function isEncodingAvailable(cache: DetailedInfinispanCache): boolean {
+  return cache.encoding.key !== EncodingType.Empty || cache.encoding.value !== EncodingType.Empty;
+}
