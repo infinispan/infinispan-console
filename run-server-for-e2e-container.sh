882f6c5f0409ecb9068056ed25b79846899f6810
Anna Manukyan <anna.mnkyn@gmail.com>
2023-10-24 13:01:10 +0200

    Changed the image URL to variable so that it is possile to run the script with any server URL.

--- run-server-for-e2e-container.sh
+++ run-server-for-e2e-container.sh
@@ -3,8 +3,10 @@ set -e
 
 ABSOLUTE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo ${ABSOLUTE_PATH}
-docker pull quay.io/infinispan-test/server:14.0.x
-docker run -d -p 11222:11222 -v ${ABSOLUTE_PATH}/scripts/identities.batch:/user-config/identities.batch -v  ${ABSOLUTE_PATH}/dist:/opt/infinispan/static/console -v  ${ABSOLUTE_PATH}/scripts/infinispan-basic-auth.xml:/opt/infinispan/server/conf/infinispan.xml -e JAVA_OPTIONS="-Xms1024m -Xmx3072m -XX:MetaspaceSize=1024m -XX:MaxMetaspaceSize=2048m" -e IDENTITIES_BATCH="/user-config/identities.batch" quay.io/infinispan-test/server:14.0.x --node-name=infinispan-4-e2e
+SERVER_IMAGE_URL="${SERVER_IMAGE_URL:-quay.io/infinispan-test/server:14.0.x}"
+
+docker pull ${SERVER_IMAGE_URL}
+docker run -d -p 11222:11222 -v ${ABSOLUTE_PATH}/scripts/identities.batch:/user-config/identities.batch -v  ${ABSOLUTE_PATH}/dist:/opt/infinispan/static/console -v  ${ABSOLUTE_PATH}/scripts/infinispan-basic-auth.xml:/opt/infinispan/server/conf/infinispan.xml -e JAVA_OPTIONS="-Xms1024m -Xmx3072m -XX:MetaspaceSize=1024m -XX:MaxMetaspaceSize=2048m" -e IDENTITIES_BATCH="/user-config/identities.batch" ${SERVER_IMAGE_URL} --node-name=infinispan-4-e2e
 docker exec $(docker ps -q -l) /opt/infinispan/bin/cli.sh install org.openjdk.nashorn:nashorn-core:15.4
 docker exec $(docker ps -q -l) /opt/infinispan/bin/cli.sh install org.ow2.asm:asm:9.4
 docker exec $(docker ps -q -l) /opt/infinispan/bin/cli.sh install org.ow2.asm:asm-commons:9.4
