25c7be6aeb0df30f139487ae573ef826f5943f0a
Katia Aresti <karesti@redhat.com>
2023-10-31 16:24:43 +0000

    ISPN-15284 Select Multi with Chips

--- src/app/Caches/Create/Features/SecuredCacheConfigurator.tsx
+++ src/app/Caches/Create/Features/SecuredCacheConfigurator.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { FormGroup, FormHelperText, HelperText, HelperTextItem } from '@patternfly/react-core';
-import { Select, SelectOption, SelectVariant } from '@patternfly/react-core/deprecated';
+import { FormGroup, FormHelperText, HelperText, HelperTextItem, SelectOptionProps } from '@patternfly/react-core';
 import { useTranslation } from 'react-i18next';
 import { ConsoleServices } from '@services/ConsoleServices';
 import { useCreateCache } from '@app/services/createCacheHook';
@@ -8,19 +7,16 @@ import { FeatureCard } from '@app/Caches/Create/Features/FeatureCard';
 import { CacheFeature } from '@services/infinispanRefData';
 import { FeatureAlert } from '@app/Caches/Create/Features/FeatureAlert';
 import { ExclamationCircleIcon } from '@patternfly/react-icons';
+import { SelectMultiWithChips } from '@app/Common/SelectMultiWithChips';
 
 const SecuredCacheConfigurator = (props: { isEnabled: boolean }) => {
   const { configuration, setConfiguration } = useCreateCache();
   const { t } = useTranslation();
   const brandname = t('brandname.brandname');
-
   const [roles, setRoles] = useState<string[]>(configuration.feature.securedCache.roles);
-
   const [loading, setLoading] = useState(true);
   const [availableRoles, setAvailableRoles] = useState<string[]>([]);
   const [error, setError] = useState<'success' | 'error' | 'default'>('default');
-
-  const [isOpenRoles, setIsOpenRoles] = useState(false);
 
   useEffect(() => {
     if (loading) {
@@ -64,12 +60,13 @@ const SecuredCacheConfigurator = (props: { isEnabled: boolean }) => {
     return roles.length > 0;
   };
 
-  const rolesOptions = () => {
-    const options = availableRoles.map((role) => <SelectOption id={role} inputId={role} key={role} value={role} />);
-    return options;
+  const rolesOptions = () : SelectOptionProps[] => {
+    const selectOptions: SelectOptionProps[] = [];
+    availableRoles.forEach((role) => selectOptions.push({value: role, children: role}));
+    return selectOptions;
   };
 
-  const onSelectRoles = (event, selection) => {
+  const onSelectRoles = (selection) => {
     if (roles.includes(selection)) setRoles(roles.filter((role) => role !== selection));
     else setRoles([...roles, selection]);
   };
@@ -84,20 +81,13 @@ const SecuredCacheConfigurator = (props: { isEnabled: boolean }) => {
       description="caches.create.configurations.feature.secured-description"
     >
       <FormGroup fieldId="select-roles" isRequired label={'Roles'}>
-        <Select
-          toggleId="roleSelector"
-          chipGroupProps={{ numChips: 4, expandedText: 'Hide', collapsedText: 'Show ${remaining}' }}
-          variant={SelectVariant.typeaheadMulti}
-          aria-label="Select Roles"
-          onToggle={() => setIsOpenRoles(!isOpenRoles)}
-          onSelect={onSelectRoles}
-          selections={roles}
-          isOpen={isOpenRoles}
-          validated={validateForm()}
-          placeholderText={t('caches.create.configurations.feature.select-roles')}
-        >
-          {rolesOptions()}
-        </Select>
+        <SelectMultiWithChips id="roleSelector"
+                              placeholder={t('caches.create.configurations.feature.select-roles')}
+                              options={rolesOptions()}
+                              selection={roles}
+                              onSelect={onSelectRoles}
+                              onClear={() => setRoles([])}
+        />
         {validateForm() === 'error' && (
           <FormHelperText>
             <HelperText>
