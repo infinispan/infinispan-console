671bd8af60e279010ee4de6e038bee235ab69561
Katia Aresti <karesti@redhat.com>
2024-01-31 13:25:51 +0100

    JDG-6691 testing library upgrade

--- test-setup.js
+++ test-setup.js
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 import './src/types/InfinispanTypes';
 import './src/services/keycloakService';
 import './src/app/services/cachesHook';
