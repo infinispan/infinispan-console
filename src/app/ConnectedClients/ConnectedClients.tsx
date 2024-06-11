7b7848892ca396c9adc40fadee604c26dc1e4446
Katia Aresti <karesti@redhat.com>
2024-06-11 15:40:44 +0200

    ISPN-16171 Connected clients protocol

--- src/app/ConnectedClients/ConnectedClients.tsx
+++ src/app/ConnectedClients/ConnectedClients.tsx
@@ -29,10 +29,9 @@ import {
   ToolbarItem,
   ToolbarItemVariant,
   ToolbarGroup,
-  EmptyStateHeader,
-  Title
+  EmptyStateHeader, Dropdown, MenuToggleElement, MenuToggle, DropdownList, DropdownItem
 } from '@patternfly/react-core';
-import { CubesIcon, SearchIcon, InfoCircleIcon } from '@patternfly/react-icons';
+import { CubesIcon, SearchIcon, InfoCircleIcon, RedoIcon } from '@patternfly/react-icons';
 import { Table, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
 import { TableErrorState } from '@app/Common/TableErrorState';
 import { useTranslation } from 'react-i18next';
@@ -42,14 +41,14 @@ import { onSearch } from '@app/utils/searchFilter';
 
 const ConnectedClients = () => {
   const { t } = useTranslation();
-  const brandname = t('brandname.brandname');
-  const { connectedClients, error, loading } = useFetchConnectedClients();
+  const { connectedClients, error, loading, setLoading } = useFetchConnectedClients();
   const [filteredConnections, setFilteredConnections] = useState<ConnectedClients[]>([]);
   const [rows, setRows] = useState<ConnectedClients[]>([]);
   const [connectionPagination, setConnectionPagination] = useState({
     page: 1,
     perPage: 10
   });
+  const [isOpen, setIsOpen] = useState(false);
   const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
@@ -75,7 +74,7 @@ const ConnectedClients = () => {
     principal: t('connected-clients.principal'),
     clientLibrary: t('connected-clients.client-library'),
     clientAddress: t('connected-clients.client-address'),
-    clientVersion: t('connected-clients.client-version'),
+    protocolVersion: t('connected-clients.protocol-version'),
     moreInfo: t('connected-clients.more-info')
   };
 
@@ -180,6 +179,18 @@ const ConnectedClients = () => {
     return row['client-library'];
   };
 
+  const displayProtocolVersion = (row) => {
+    if (row['protocol-version'] === null) {
+      return (
+        <TextContent>
+          <Text component={TextVariants.small}>{t('connected-clients.null')}</Text>
+        </TextContent>
+      );
+    }
+
+    return row['protocol-version'];
+  };
+
   const displayMoreInfo = (row: ConnectedClients) => {
     const description = (
       <DescriptionList isHorizontal>
@@ -228,9 +239,15 @@ const ConnectedClients = () => {
           </DescriptionListDescription>
         </DescriptionListGroup>
         <DescriptionListGroup>
+          <DescriptionListTerm>{t('connected-clients.client-version')}</DescriptionListTerm>
+          <DescriptionListDescription>
+            <Label color="blue">{row['client-version'] == null? t('connected-clients.null') : row['client-version']}</Label>
+          </DescriptionListDescription>
+        </DescriptionListGroup>
+        <DescriptionListGroup>
           <DescriptionListTerm>{t('connected-clients.local-address')}</DescriptionListTerm>
           <DescriptionListDescription>
-            <Label color="blue">{row['local-address']}</Label>
+            <Label color="blue">{row['local-address'] == null? t('connected-clients.null') : row['local-address']}</Label>
           </DescriptionListDescription>
         </DescriptionListGroup>
       </DescriptionList>
@@ -269,83 +286,112 @@ const ConnectedClients = () => {
       );
     }
 
+    if (connectedClients.length === 0) {
+      return (
+        <Card>
+          <CardBody>
+            {emptyPage}
+          </CardBody>
+        </Card>
+      )
+    }
+
     return (
-      <React.Fragment>
-        {connectedClients.length === 0 ? (
-          emptyPage
-        ) : (
-          <Card>
-            <CardBody>
-              <Toolbar
-                style={{ paddingBottom: '1rem' }}
-                id="connections-table-toolbar"
-                className={'connections-table-display'}
-              >
-                <ToolbarContent>
-                  {buildSearch}
-                  <ToolbarItem variant={ToolbarItemVariant.pagination}>{pagination}</ToolbarItem>
-                </ToolbarContent>
-              </Toolbar>
-              <Table className={'connections-table'} aria-label={'connections-table-label'} variant={'compact'}>
-                <Thead>
-                  <Tr>
-                    <Th style={{ width: '15%' }} colSpan={1}>
-                      {columnNames.nodeName}
-                    </Th>
-                    <Th style={{ width: '10%' }} colSpan={1}>
-                      {columnNames.principal}
-                    </Th>
-                    <Th colSpan={1}>{columnNames.clientLibrary}</Th>
-                    <Th style={{ width: '12%' }} colSpan={1}>
-                      {columnNames.clientAddress}
-                    </Th>
-                    <Th style={{ width: '12%' }} colSpan={1}>
-                      {columnNames.clientVersion}
-                    </Th>
-                    <Th style={{ width: '5%' }} />
-                  </Tr>
-                </Thead>
-                <Tbody>
-                  {filteredConnections.length == 0 ? (
-                    <Tr>
-                      <Td colSpan={4}>
-                        <Bullseye>
-                          <EmptyState variant={EmptyStateVariant.sm}>
-                            <EmptyStateHeader
-                              titleText={<>{t('connected-clients.no-filtered-connections')}</>}
-                              icon={<EmptyStateIcon icon={SearchIcon} />}
-                              headingLevel="h2"
-                            />
-                            <EmptyStateBody>{t('connected-clients.no-filtered-connections-body')}</EmptyStateBody>
-                          </EmptyState>
-                        </Bullseye>
-                      </Td>
+      <Card>
+        <CardBody>
+          <Toolbar
+            style={{ paddingBottom: '1rem' }}
+            id="connections-table-toolbar"
+            className={'connections-table-display'}
+          >
+            <ToolbarContent>
+              {buildSearch}
+              <ToolbarItem variant={ToolbarItemVariant.pagination}>{pagination}</ToolbarItem>
+            </ToolbarContent>
+          </Toolbar>
+          <Table className={'connections-table'} aria-label={'connections-table-label'} variant={'compact'}>
+            <Thead>
+              <Tr>
+                <Th style={{ width: '15%' }} colSpan={1}>
+                  {columnNames.nodeName}
+                </Th>
+                <Th style={{ width: '10%' }} colSpan={1}>
+                  {columnNames.principal}
+                </Th>
+                <Th colSpan={1}>{columnNames.clientLibrary}</Th>
+                <Th style={{ width: '12%' }} colSpan={1}>
+                  {columnNames.clientAddress}
+                </Th>
+                <Th style={{ width: '12%' }} colSpan={1}>
+                  {columnNames.protocolVersion}
+                </Th>
+                <Th style={{ width: '5%' }} />
+              </Tr>
+            </Thead>
+            <Tbody>
+              {filteredConnections.length == 0 ? (
+                <Tr>
+                  <Td colSpan={4}>
+                    <Bullseye>
+                      <EmptyState variant={EmptyStateVariant.sm}>
+                        <EmptyStateHeader
+                          titleText={<>{t('connected-clients.no-filtered-connections')}</>}
+                          icon={<EmptyStateIcon icon={SearchIcon} />}
+                          headingLevel="h2"
+                        />
+                        <EmptyStateBody>{t('connected-clients.no-filtered-connections-body')}</EmptyStateBody>
+                      </EmptyState>
+                    </Bullseye>
+                  </Td>
+                </Tr>
+              ) : (
+                rows.map((row) => {
+                  return (
+                    <Tr key={row.id}>
+                      <Td dataLabel={columnNames.nodeName}>{row['server-node-name']}</Td>
+                      <Td dataLabel={columnNames.principal}>{displayPrincipal(row)}</Td>
+                      <Td dataLabel={columnNames.clientLibrary}>{displayClientLibrary(row)}</Td>
+                      <Td dataLabel={columnNames.clientAddress}>{displayClientAddress(row)}</Td>
+                      <Td dataLabel={columnNames.protocolVersion}>{displayProtocolVersion(row)}</Td>
+                      <Td dataLabel={columnNames.moreInfo}>{displayMoreInfo(row)}</Td>
                     </Tr>
-                  ) : (
-                    rows.map((row) => {
-                      return (
-                        <Tr key={row.id}>
-                          <Td dataLabel={columnNames.nodeName}>{row['server-node-name']}</Td>
-                          <Td dataLabel={columnNames.principal}>{displayPrincipal(row)}</Td>
-                          <Td dataLabel={columnNames.clientLibrary}>{displayClientLibrary(row)}</Td>
-                          <Td dataLabel={columnNames.clientAddress}>{displayClientAddress(row)}</Td>
-                          <Td dataLabel={columnNames.clientVersion}>{displayClientVersion(row)}</Td>
-                          <Td dataLabel={columnNames.moreInfo}>{displayMoreInfo(row)}</Td>
-                        </Tr>
-                      );
-                    })
-                  )}
-                </Tbody>
-              </Table>
-              <Toolbar id="connections-table-toolbar" className={'connections-table-display'}>
-                <ToolbarItem variant={ToolbarItemVariant.pagination}>{pagination}</ToolbarItem>
-              </Toolbar>
-            </CardBody>
-          </Card>
-        )}
-      </React.Fragment>
-    );
-  };
+                  );
+                })
+              )}
+            </Tbody>
+          </Table>
+          <Toolbar id="connections-table-toolbar" className={'connections-table-display'}>
+            <ToolbarItem variant={ToolbarItemVariant.pagination}>{pagination}</ToolbarItem>
+          </Toolbar>
+        </CardBody>
+      </Card>
+    )
+  }
+
+  const displayActions = (
+    <ToolbarGroup align={{ default: 'alignRight' }}>
+      <ToolbarItem>
+        <Dropdown
+          isOpen={isOpen}
+          onSelect={() => setIsOpen(false)}
+          onOpenChange={(isOpen: boolean) => setIsOpen(isOpen)}
+          toggle={(toggleRef: React.Ref<MenuToggleElement>) => (
+            <MenuToggle ref={toggleRef} data-cy="aclActions" onClick={() => setIsOpen(!isOpen)} isExpanded={isOpen}>
+              {t('common.actions.actions')}
+            </MenuToggle>
+          )}
+          ouiaId="conectedClientsDropdown"
+          shouldFocusToggleOnSelect
+        >
+          <DropdownList>
+            <DropdownItem value={0} key="refreshAction" data-cy="refreshAction" onClick={() => setLoading(true)} icon={<RedoIcon />}>
+              {t('common.actions.refresh')}
+            </DropdownItem>
+          </DropdownList>
+        </Dropdown>
+      </ToolbarItem>
+    </ToolbarGroup>
+  );
 
   return (
     <React.Fragment>
@@ -358,6 +404,7 @@ const ConnectedClients = () => {
                 <Text component={TextVariants.p}>{t('connected-clients.connected-clients-description')}</Text>
               </TextContent>
             </ToolbarItem>
+            {displayActions}
           </ToolbarContent>
         </Toolbar>
       </PageSection>
