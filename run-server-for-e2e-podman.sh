d2ba95923aaccf9f986283511c59342f15af0b88
Katia Aresti <karesti@redhat.com>
2024-08-19 13:48:34 +0200

    Upgrade server version to 15.0

--- run-server-for-e2e-podman.sh
+++ run-server-for-e2e-podman.sh
@@ -3,7 +3,7 @@ set -e
 
 ABSOLUTE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo ${ABSOLUTE_PATH}
-SERVER_IMAGE_URL="${SERVER_IMAGE_URL:-quay.io/infinispan-test/server:main}"
+SERVER_IMAGE_URL="${SERVER_IMAGE_URL:-quay.io/infinispan-test/server:15.0.x}"
 
 podman pull ${SERVER_IMAGE_URL}
 podman run -d -p 11222:11222 -v ${ABSOLUTE_PATH}/scripts/identities.batch:/user-config/identities.batch -v  ${ABSOLUTE_PATH}/dist:/opt/infinispan/static/console -v  ${ABSOLUTE_PATH}/scripts/infinispan-basic-auth.xml:/opt/infinispan/server/conf/infinispan.xml -e JAVA_OPTIONS="-Xms1024m -Xmx3072m -XX:MetaspaceSize=1024m -XX:MaxMetaspaceSize=2048m" -e IDENTITIES_BATCH="/user-config/identities.batch" ${SERVER_IMAGE_URL} --node-name=infinispan-4-e2e
