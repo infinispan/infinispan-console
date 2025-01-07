d7879e458fe193db5aceedd775ecb2c869a546a7
Katia Aresti <karesti@redhat.com>
2025-01-07 13:35:55 +0100

    Change how to run the server for tests

--- run-server-for-e2e-container.sh
+++ run-server-for-e2e-container.sh
@@ -1,26 +1,32 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
+if command -v docker &> /dev/null; then
+    echo "Using Docker"
+    DOCKER_COMMAND="docker"
+else
+    echo "Docker not found, using Podman"
+    DOCKER_COMMAND="podman"
+fi
 
 ABSOLUTE_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo ${ABSOLUTE_PATH}
 SERVER_IMAGE_URL="${SERVER_IMAGE_URL:-quay.io/infinispan-test/server:main}"
 
-docker pull ${SERVER_IMAGE_URL}
-docker run --name ispn-lon -d -p 11222:11222 -v ${ABSOLUTE_PATH}/scripts/identities.batch:/user-config/identities.batch -v  ${ABSOLUTE_PATH}/dist:/opt/infinispan/static/console -v  ${ABSOLUTE_PATH}/scripts/infinispan-basic-auth.xml:/opt/infinispan/server/conf/infinispan.xml -e JAVA_OPTIONS="-Xms1024m -Xmx3072m -XX:MetaspaceSize=1024m -XX:MaxMetaspaceSize=2048m -Dinfinispan.site.name=LON -Djgroups.mcast_port=46656" -e IDENTITIES_BATCH="/user-config/identities.batch" ${SERVER_IMAGE_URL} --node-name=infinispan-4-e2e
-docker run --name ispn-nyc -d -p 31222:11222 -v ${ABSOLUTE_PATH}/scripts/identities.batch:/user-config/identities.batch -v  ${ABSOLUTE_PATH}/dist:/opt/infinispan/static/console -v  ${ABSOLUTE_PATH}/scripts/infinispan-basic-auth.xml:/opt/infinispan/server/conf/infinispan.xml -e JAVA_OPTIONS="-Xms1024m -Xmx3072m -XX:MetaspaceSize=1024m -XX:MaxMetaspaceSize=2048m -Dinfinispan.site.name=NYC -Djgroups.mcast_port=46666" -e IDENTITIES_BATCH="/user-config/identities.batch" ${SERVER_IMAGE_URL} --node-name=infinispan-4-e2e
+$DOCKER_COMMAND pull ${SERVER_IMAGE_URL}
+$DOCKER_COMMAND run --name ispn-lon -d -p 11222:11222 -v ${ABSOLUTE_PATH}/scripts/identities.batch:/user-config/identities.batch -v ${ABSOLUTE_PATH}/dist:/opt/infinispan/static/console -v ${ABSOLUTE_PATH}/scripts/e2eTestsConfigLON.xml:/user-config/e2eTestsConfigLON.xml -e JAVA_OPTIONS="-Xms1024m -Xmx3072m -XX:MetaspaceSize=1024m -XX:MaxMetaspaceSize=2048m -Dinfinispan.site.name=LON -Djgroups.mcast_port=46656" -e IDENTITIES_BATCH="/user-config/identities.batch" ${SERVER_IMAGE_URL} --node-name=infinispan-4-lon-e2e -c infinispan-xsite.xml -c "/user-config/e2eTestsConfigLON.xml"
+$DOCKER_COMMAND run --name ispn-nyc -d -p 31222:11222 -v ${ABSOLUTE_PATH}/scripts/identities.batch:/user-config/identities.batch -v ${ABSOLUTE_PATH}/dist:/opt/infinispan/static/console -v ${ABSOLUTE_PATH}/scripts/e2eTestsConfigNYC.xml:/user-config/e2eTestsConfigNYC.xml -e JAVA_OPTIONS="-Xms1024m -Xmx3072m -XX:MetaspaceSize=1024m -XX:MaxMetaspaceSize=2048m -Dinfinispan.site.name=NYC -Djgroups.mcast_port=46666" -e IDENTITIES_BATCH="/user-config/identities.batch" ${SERVER_IMAGE_URL} --node-name=infinispan-4-nyc-e2e -c infinispan-xsite.xml -c "/user-config/e2eTestsConfigNYC.xml"
 
 #Adding nashorn libraries to both containers
-for containerId in $(docker ps -q)
+for containerId in $($DOCKER_COMMAND ps -q)
 do
-  docker exec -it $containerId /opt/infinispan/bin/cli.sh install org.openjdk.nashorn:nashorn-core:15.4
-  docker exec -it $containerId /opt/infinispan/bin/cli.sh install org.ow2.asm:asm:9.4
-  docker exec -it $containerId /opt/infinispan/bin/cli.sh install org.ow2.asm:asm-commons:9.4
-  docker exec -it $containerId /opt/infinispan/bin/cli.sh install org.ow2.asm:asm-tree:9.4
-  docker exec -it $containerId /opt/infinispan/bin/cli.sh install org.ow2.asm:asm-util:9.4
+  $DOCKER_COMMAND exec -it $containerId /opt/infinispan/bin/cli.sh install org.openjdk.nashorn:nashorn-core:15.4
+  $DOCKER_COMMAND exec -it $containerId /opt/infinispan/bin/cli.sh install org.ow2.asm:asm:9.4
+  $DOCKER_COMMAND exec -it $containerId /opt/infinispan/bin/cli.sh install org.ow2.asm:asm-commons:9.4
+  $DOCKER_COMMAND exec -it $containerId /opt/infinispan/bin/cli.sh install org.ow2.asm:asm-tree:9.4
+  $DOCKER_COMMAND exec -it $containerId /opt/infinispan/bin/cli.sh install org.ow2.asm:asm-util:9.4
 done
 
 #Restarting containers
-docker restart $(docker ps -q)
+$DOCKER_COMMAND restart $(docker ps -q)
 
 sleep 20
 #Creating data
