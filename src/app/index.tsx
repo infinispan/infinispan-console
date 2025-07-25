05812fe7fdda7f5fb30234e4265ac20455dfbbf8
Katia Aresti <karesti@redhat.com>
2025-07-25 19:57:29 +0200

    [#610] Fixes correct way to import chart css

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '@patternfly/react-core/dist/styles/base.css';
+import '@patternfly/patternfly/patternfly-charts.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { AppLayout } from '@app/AppLayout/AppLayout';
 import { AppRoutes } from '@app/routes';
