444edabf11a5014cd1a4a8c12bc0843e7627b378
Katia Aresti <karestig@ibm.com>
2026-04-14 17:20:59 +0200

    [#544] Move twitter logo to X

--- src/app/About/About.tsx
+++ src/app/About/About.tsx
@@ -21,11 +21,22 @@ import {
   GithubIcon,
   OutlinedCommentsIcon,
   StackOverflowIcon,
-  TwitterSquareIcon,
 } from '@patternfly/react-icons';
 import { useFetchVersion } from '@app/hooks/serverHook';
 import { useTranslation } from 'react-i18next';
 import './About.css';
+
+const XIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    fill="currentColor"
+    width="1em"
+    height="1em"
+  >
+    <path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z" />
+  </svg>
+);
 
 const About = (props: { isModalOpen: boolean; closeModal: () => void }) => {
   const { t } = useTranslation();
@@ -36,7 +47,7 @@ const About = (props: { isModalOpen: boolean; closeModal: () => void }) => {
   const infinispanZulipLink = 'https://infinispan.zulipchat.com/';
   const infinispanStackOverflowLink =
     'https://stackoverflow.com/questions/tagged/?tagnames=infinispan&sort=newest';
-  const infinispanTwitterLink = 'https://x.com/infinispan/';
+  const infinispanXLink = 'https://x.com/infinispan/';
   const infinispanFacebookLink = 'https://www.facebook.com/infinispan/';
   const description1 = t('welcome-page.description1', { brandname: brandname });
   const description2 = t('welcome-page.description2', { brandname: brandname });
@@ -126,14 +137,14 @@ const About = (props: { isModalOpen: boolean; closeModal: () => void }) => {
 
             <FlexItem>
               <Button
-                aria-label={'Twitter'}
+                aria-label={'X'}
                 component={'a'}
-                href={infinispanTwitterLink}
+                href={infinispanXLink}
                 variant={ButtonVariant.link}
                 target="_blank"
               >
                 <Icon size="md">
-                  <TwitterSquareIcon />
+                  <XIcon />
                 </Icon>
               </Button>
             </FlexItem>
