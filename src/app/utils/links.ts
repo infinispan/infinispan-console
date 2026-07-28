663c4f511ab9a4d6cc870b732376dc0f923f0962
Peter Ondruska <ondruska@users.noreply.github.com>
2026-07-28 17:12:33 +0200

    Replace link to client downloads fix #848

--- src/app/utils/links.ts
+++ src/app/utils/links.ts
@@ -1,5 +1,6 @@
-export const hotRodClientsLink = 'https://infinispan.org/hotrod-clients/';
+export const hotRodClientsLink = 'https://infinispan.org/downloads/clients/';
 export const aboutLink = 'https://infinispan.org/get-started/';
-export const tutorialsLink = 'https://github.com/infinispan/infinispan-simple-tutorials';
+export const tutorialsLink =
+  'https://github.com/infinispan/infinispan-simple-tutorials';
 export const blogLink = 'https://infinispan.org/blog/';
 export const apacheLicenseLink = 'https://www.apache.org/licenses/LICENSE-2.0';
