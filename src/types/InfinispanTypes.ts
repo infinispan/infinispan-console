b79ce3346a79d86f7b63ba07f11d52cab41eea09
Katia Aresti <karestig@ibm.com>
2026-08-18 12:49:32 +0200

    [#868] Update by query feature

--- src/types/InfinispanTypes.ts
+++ src/types/InfinispanTypes.ts
@@ -93,6 +93,7 @@ import type { Counter as _Counter, CounterConfig as _CounterConfig } from './cou
 import type {
   SearchResult as _SearchResult,
   DeleteByQueryResult as _DeleteByQueryResult,
+  UpdateByQueryResult as _UpdateByQueryResult,
   IndexStat as _IndexStat,
   QueryStat as _QueryStat,
   SearchStats as _SearchStats,
@@ -185,6 +186,7 @@ declare global {
   // search
   type SearchResult = _SearchResult;
   type DeleteByQueryResult = _DeleteByQueryResult;
+  type UpdateByQueryResult = _UpdateByQueryResult;
   type IndexStat = _IndexStat;
   type QueryStat = _QueryStat;
   type SearchStats = _SearchStats;
